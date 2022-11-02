5a8597c94db6a191dd180fcb3fd8c0d2a0e18fbc
darwin <d.f.winzi2006@gmail.com>
2022-11-02 16:49:49 +0700

    update

--- pages/catalog/index.tsx
+++ pages/catalog/index.tsx
@@ -3,10 +3,11 @@ import Navbar from '../../components/bar/navbar';
 import Styles from '../../styles/catalog.module.css';
 import Sidebar from '../../components/bar/sidebar';
 import { useEffect, useState } from 'react';
-// import dataFetch from '../api/fetch';
+import Loading from '../../components/loading';
+import dataFetch from '../api/fetch';
 
 interface data {
-  title: string;
+  category_name: string;
   id: number;
 }
 
@@ -16,8 +17,7 @@ const Catalog: NextPage = () => {
 
   useEffect(() => {
     setLoading(true);
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((res) => res.json())
+    dataFetch()
       .then((d) => {
         setData(d);
         setLoading(false);
@@ -30,14 +30,15 @@ const Catalog: NextPage = () => {
     <div className={Styles.container}>
       <Sidebar />
       <Navbar />
+      {/* <Loading/> */}
       {loading ? (
-        <p>loading...</p>
+        <Loading/>
       ) : !data ? (
         <p>no data</p>
       ) : (
         <div className={Styles.main}>
-          {data.map(({ title, id }) => (
-            <p key={id}>{id}. title :<br />{title}</p>
+          {data.map(({ category_name, id }) => (
+            <p key={id}>{id}. title :<br />{category_name}</p>
           ))}
         </div>
       )}
