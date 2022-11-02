c432a891696ffa6201b33cc2938831df330141c7
darwin <d.f.winzi2006@gmail.com>
2022-11-02 20:08:21 +0700

    custom error update

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,18 +1,26 @@
 import { NextPage } from 'next';
+import Link from 'next/link';
 
 const Custom404: NextPage = () => {
   return (
     <div
       style={{
         display: 'flex',
-        justifyContent: 'center',
+        paddingTop:'30vh',
         alignItems: 'center',
-        height:'100vh',
-        backgroundColor:'rgb(0, 152, 103, 0.3)'
+        flexDirection:'column',
+        height: '100vh',
+        backgroundColor: 'rgb(0, 152, 103, 0.1)',
       }}
     >
-      <div style={{color:'red'}}>
-        <h1>404 | Page Not Found</h1>
+      <div style={{ textAlign: 'center' }} className='animated'>
+        <h2>404 | Page Not Found</h2>
+        <h2>404 | Page Not Found</h2>
+      </div>
+      <div style={{}}>
+      <Link href={'/catalog'}>
+        <a style={{ cursor: 'pointer' }}>Back?</a>
+      </Link>        
       </div>
     </div>
   );
